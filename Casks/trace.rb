25e71e17a550c30c303a160955a5bd6ea447eaea
Arjun Komath <arjun@hey.com>
2025-08-14 19:57:26 +1000

    Publish Trace

---
+++ Casks/trace.rb
@@ -0,0 +1,27 @@
+cask "trace" do
+  version "1.2.1"
+  sha256 "34703c09c4a0020b30ff67aa3039f5b9357751baea90f8f16f5b4f4570f44cfb"
+
+  url "https://github.com/arjunkomath/trace/releases/download/v#{version}/Trace-#{version}.dmg",
+      verified: "github.com/arjunkomath/trace/"
+  name "Trace"
+  desc "Spotlight alternative and shortcut toolkit for macOS"
+  homepage "https://github.com/arjunkomath/trace"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sequoia"
+
+  app "Trace.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.techulus.trace",
+    "~/Library/Caches/com.techulus.trace",
+    "~/Library/HTTPStorages/com.techulus.trace",
+    "~/Library/Preferences/com.techulus.trace.plist",
+    "~/Library/Saved Application State/com.techulus.trace.savedState",
+  ]
+end
